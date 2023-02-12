954e9a707675132e9fdac9d1a3a4d56c3273f32e
leechulhan <dhfhfkwhdk@naver.com>
2023-02-12 23:24:27 +0900

    TownNews Project Inital Commit

---
+++ back/townNews/src/main/java/com/town/townNews/TownNewsApplication.java
@@ -0,0 +1,14 @@
+package com.town.townNews;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TownNewsApplication.class, args);
+	}
+
+}
---
+++ back/townNews/src/test/java/com/town/townNews/TownNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.town.townNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TownNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
